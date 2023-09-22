4222fb8f44f51c66b3bbbd80eb4eca17a7cb56eb
Phanatagama <phanatagama@gmail.com>
2023-09-22 21:36:56 +0700

    feat(data): create Project.kt

---
+++ app/src/main/java/com/example/myrecyclerview/data/model/Project.kt
@@ -0,0 +1,10 @@
+package com.example.myrecyclerview.data.model
+
+data class Project(
+    val id: String,
+    val name: String,
+    val description: String,
+    val language: String,
+    val stars: Int,
+    val issues: Int
+)
