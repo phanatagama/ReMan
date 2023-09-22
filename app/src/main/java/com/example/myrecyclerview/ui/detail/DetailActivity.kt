3eef0aa333ef3debaec26ed062db4958b1b1f046
Phanatagama <phanatagama@gmail.com>
2023-09-22 21:34:54 +0700

    feat(activity): modify DetailActivity.kt

--- app/src/main/java/com/example/myrecyclerview/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/ui/detail/DetailActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myrecyclerview
+package com.example.myrecyclerview.ui.detail
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,37 +7,53 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.RequestOptions
+import com.example.myrecyclerview.R
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class DetailActivity : AppCompatActivity() {
-    companion object {
-        const val EXTRA_DETAIL = "extra_detail"
-        const val EXTRA_TITLE = "extra_title"
-        const val EXTRA_IMG = "extra_img"
-
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
+//        setTheme(R.style.Theme_MyRecyclerView)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
-        supportActionBar?.title = "Detail Komik"
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
+//        supportActionBar?.title = "Detail Komik"
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
         val imgCover: ImageView = findViewById(R.id.img_item_photo)
         val tvTitle: TextView = findViewById(R.id.tv_item_name)
         val tvDetail: TextView = findViewById(R.id.tv_item_detail)
         val btnFavorite: Button = findViewById(R.id.btn_set_favorite)
-        val btnShare: Button = findViewById(R.id.btn_set_share)
+        val btnShare: Button = findViewById(R.id.action_share)
 
-        val img = intent.getIntExtra(EXTRA_IMG,0)
+        val img = intent.getIntExtra(EXTRA_IMG, 0)
         val title = intent.getStringExtra(EXTRA_TITLE)
         val detail = intent.getStringExtra(EXTRA_DETAIL)
+        val info = intent.getStringExtra(EXTRA_INFO)
 
-        tvTitle.text = title
+        val collapseLayout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+        collapseLayout.title = title
+        collapseLayout.setExpandedTitleColor(ContextCompat.getColor(this, R.color.white))
+        collapseLayout.setCollapsedTitleTextColor(ContextCompat.getColor(this,R.color.white))
+
+        tvTitle.text = info
         tvDetail.text = detail
         Glide.with(this)
             .load(img)
-            .apply(RequestOptions().dontTransform())
+            .transition(withCrossFade())
+            .apply(RequestOptions().transform())
             .into(imgCover)
         btnFavorite.setOnClickListener {
             Toast.makeText(this, "Favorit " + title, Toast.LENGTH_SHORT).show()
@@ -50,5 +66,12 @@ class DetailActivity : AppCompatActivity() {
             }
             startActivity(Intent.createChooser(shareIntent, "Share comic to.."))
         }
+    }
+
+    companion object {
+        const val EXTRA_DETAIL = "extra_detail"
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_IMG = "extra_img"
+        const val EXTRA_INFO = "extra_info"
     }
 }
