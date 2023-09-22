af6559d6d06c43e50c9aa4e4e0e06d32bfa67f73
Phanatagama <phanatagama@gmail.com>
2023-09-22 21:35:16 +0700

    feat(activity): modify MainActivity.kt

--- app/src/main/java/com/example/myrecyclerview/ui/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/ui/MainActivity.kt
@@ -1,28 +1,37 @@
-package com.example.myrecyclerview
+package com.example.myrecyclerview.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myrecyclerview.R
+import com.example.myrecyclerview.data.ComicsData
+import com.example.myrecyclerview.data.model.Comic
+import com.example.myrecyclerview.ui.about.AboutActivity
+import com.example.myrecyclerview.ui.detail.DetailActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvComic: RecyclerView
     private var list: ArrayList<Comic> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rvComic = findViewById(R.id.rv_comic)
-        rvComic.setHasFixedSize(true)
 
         list.addAll(ComicsData.listData)
         showRecyclerList()
     }
+
     private fun showRecyclerList() {
+        rvComic.setHasFixedSize(true)
         rvComic.layoutManager = LinearLayoutManager(this)
         val listComicAdapter = ListComicAdapter(list)
         rvComic.adapter = listComicAdapter
@@ -47,6 +56,7 @@ class MainActivity : AppCompatActivity() {
     private fun showSelectedHero(comic: Comic) {
         val moveWithDataIntent = Intent(this@MainActivity, DetailActivity::class.java)
         moveWithDataIntent.putExtra(DetailActivity.EXTRA_TITLE, comic.title)
+        moveWithDataIntent.putExtra(DetailActivity.EXTRA_INFO, comic.info)
         moveWithDataIntent.putExtra(DetailActivity.EXTRA_DETAIL, comic.detail)
         moveWithDataIntent.putExtra(DetailActivity.EXTRA_IMG, comic.photo)
         startActivity(moveWithDataIntent)
